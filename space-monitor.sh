ba1b0d9e7490a51a3eae8b2bd33abcffedae043d
harshad8782 <85127402+harshad8782@users.noreply.github.com>
2026-05-01 13:30:36 +0530

    updated space monitor script

--- space-monitor.sh
+++ space-monitor.sh
@@ -1,3 +1,21 @@
+# #!/bin/bash
+
+# # MOnitor free space in disk
+# FU=$(df -H | egrep -v "Filesystem|tmpfs" | grep "sda2" | awk '{print $5}' | tr -d %)
+# TO="your_mail@gmail.com"
+
+# # Send alert to mail if disk space is low!
+# # For sending mail we nned to setup postfix in our server or machine
+
+# if [[ $FU -ge 20 ]]
+# then
+# 	echo "Warning, disk space is low - $FU %" | mail -s "DISK SPACE ALERT IN SERVER" $TO
+# else
+# 	echo "All Good"
+# fi
+
+
+
 #!/bin/bash
 
 # MOnitor free space in disk
